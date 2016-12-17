ff9020c81adc6104c27b0494db9dce850f8cc1b5
jgper <jgper@DESKTOP-VAF4E9Q>
2016-12-17 18:41:18 +0100

    CARGA
    -Prueba de almacenamiento de informacion basica distrito-barrio.

--- DATOS/src/main/java/preprocesamiento/Almacenar.java
+++ DATOS/src/main/java/preprocesamiento/Almacenar.java
@@ -48,27 +48,36 @@ public class Almacenar {
 	 * 
 	 */
 	private void generarColeccion() throws IOException{
-		CsvReader distritos_barrios = new CsvReader(".\\documents\\200076-1-padron.csv");
+		collection.drop();
+		
+		CsvReader distritos_barrios = new CsvReader(".\\documents\\200076-1-padron.csv",';');
 		distritos_barrios.readHeaders();
 
 		//		private List<Document> distritos;
 		List<Document> distritos = new ArrayList<Document>();
 		int index = 0;
 		while (distritos_barrios.readRecord()){
-			if(distritos.isEmpty() || (index = buscarDistrito(distritos, distritos_barrios.get("COD_DISTRITO")))<0){
-				distritos.add(new Document("_id", distritos_barrios.get("COD_DISTRITO"))
-						.append("nombre", distritos_barrios.get("DESC_DISTRITO"))
-						.append("barrios", new ArrayList<Document>()
-								.add(new Document()
-										.append("id_barrio",  distritos_barrios.get("COD_BARRIO"))//VER SI ES ESTE CODIGO O EL DE DIST_BARRIO
-										.append("nombre", distritos_barrios.get("DESC_BARRIO")))));
+			if(distritos.isEmpty() || (index = buscarDistrito_Barrio(distritos, distritos_barrios.get("COD_DISTRITO"), "_id"))<0){
+				Document dist = new Document("_id", distritos_barrios.get("COD_DISTRITO"))
+						.append("nombre", distritos_barrios.get("DESC_DISTRITO"));
+				List<Document> barrios = new ArrayList<>();
+				barrios.add((new Document()
+						.append("id_barrio",  distritos_barrios.get("COD_BARRIO"))//VER SI ES ESTE CODIGO O EL DE DIST_BARRIO
+						.append("nombre", distritos_barrios.get("DESC_BARRIO"))));
+				dist.append("barrios", barrios);
+				distritos.add(dist);		
 			}else{
 				Document dist = distritos.get(index);
+				List<Document> barrios = (List<Document>) dist.get("barrios");//VER SI ASI VALE O HAY QUE MACHACAR LISTA EN DIST!!
+				if(buscarDistrito_Barrio(barrios, distritos_barrios.get("COD_BARRIO"), "id_barrio")<0){
 				Document barrio = new Document()
 						.append("id_barrio",  distritos_barrios.get("COD_BARRIO"))//VER SI ES ESTE CODIGO O EL DE DIST_BARRIO
 						.append("nombre", distritos_barrios.get("DESC_BARRIO"));
-				((List<Document>) dist.get("barrios")).add(barrio);//VER SI ASI VALE O HAY QUE MACHACAR LISTA EN DIST!!
-				distritos.add(index, dist);//actualizamos
+				barrios.add(barrio);
+				dist.replace("barrios", (List<Document>) dist.get("barrios"), barrios);
+				distritos.remove(index);//actualizamos
+				distritos.add(dist);
+				}
 			}
 			//TERMINAR AÑADIENDO PADRON!! VER ARCHIVO CON SIGNIFICADO ID EDAD ETC
 		}
@@ -76,10 +85,10 @@ public class Almacenar {
 		collection.insertMany(distritos);//insertamos los distritos
 	}
 
-	private int buscarDistrito(List<Document> distritos, String code){
-		for(Document dist:distritos){
-			if(dist.getObjectId("_id").toString().equals(code)){
-				return distritos.indexOf(dist);
+	private int buscarDistrito_Barrio(List<Document> distritos_barrios, String code, String id){
+		for(Document dist_bar:distritos_barrios){
+			if(dist_bar.get(id).toString().equals(code)){
+				return distritos_barrios.indexOf(dist_bar);
 			}
 		}
 		return -1;
@@ -88,5 +97,6 @@ public class Almacenar {
 	public static void main(String[] args) throws JSONException, FileNotFoundException, IOException, ParseException  {
 		Almacenar alm = new Almacenar();
 		alm.generarColeccion();
+		alm.client.close();
 	}
 }
